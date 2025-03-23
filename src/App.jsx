34b89367b4df5cbc6033d1cc33a2f0feb8986853
Shohanur-rahomman <mdshohanur757@gmail.com>
2025-03-23 12:47:53 +0600

    'second'

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import Todo from './Todo';
 import './App.css'
 
 function App() {
-
-
+  
+  const time = 50;
+  
   return (
     
     <div className='container'>
       
       <h1>Vite + React</h1>
       
-      <Todo tusk="learn react" isDone={true}></Todo>
+      <Todo tusk="learn react" isDone={true} time={time}></Todo>
       <Todo tusk="learn javaScript" isDone={false}></Todo>
-      <Todo tusk="shower ok" isDone={true}></Todo>
+      <Todo tusk="shower ok" isDone={true} time={time}></Todo>
     </div>
   )
 }
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,29 +1,62 @@
-// export default function Todo({tusk,isDone}) {
-//     if (isDone) {
-//         return <li>Done : { tusk}</li>
-//     } else {
-//         return <li> do now : { tusk}</li>
-//   }
-// }
-// export default function Todo({tusk,isDone}) {
-//     return (
-//         <li>
-//             {isDone ? `Done: ${tusk}` : `Do now: ${tusk}`}
-//         </li>
-//     )
-// }
-// export default function Todo ({tusk,isDone}) {
-//     return (
-//         <li>
-//             {isDone ? `Done : ${tusk}` : `Do now : ${tusk}`}
-//         </li>
-//     )
-// }
-
-export default function Todo({tusk,isDone}) {
+function Todo({tusk,isDone}) {
     if (isDone) {
-        return <li> Done :{ tusk}</li>
+        return <li>Done : { tusk}</li>
     } else {
-        return <li>Do now : { tusk}</li>
+        return <li> do now : { tusk}</li>
+  }
+}
+ function Todo1({tusk,isDone}) {
+    return (
+        <li>
+            {isDone ? `Done: ${tusk}` : `Do now: ${tusk}`}
+        </li>
+    )
+}
+ function Todo2 ({tusk,isDone}) {
+    return (
+        <li>
+            {isDone ? `Done : ${tusk}` : `Do now : ${tusk}`}
+        </li>
+    )
+}
+ function Todo3({ tusk, isDone, time = 0}) {
+    return isDone ? <li>Done : {tusk} time : {time} </li> : <li>{ tusk}</li>
+}
+
+ function Todo4({ tusk, isDone, time = 0 }) {
+    if (isDone) {
+        return <li> Done :{tusk} Duration: {time}</li>
+    } else {
+        return <li>Do now : {tusk}</li>
     }
+}
+
+ function Todo5({tusk,isDone,time= 0}) {
+    return isDone && <li> Done {tusk} time : {time}</li>
+}
+
+function Todo6({tusk,isDone,time= 0}) {
+    return !isDone || <li> Done {tusk} time : {time}</li>
+}
+
+ function Todo7({tusk,isDone}) {
+    if (isDone) {
+        return null
+    } else {
+        return <li> do now : { tusk}</li>
+  }
+}
+
+export default function Todo8({ tusk, isDone, time }) {
+    
+    const displayTime = time ? time : 100
+    
+    let result;
+
+    if (isDone) {
+        result = <li>Done : {tusk} time : { displayTime}</li>
+    } else {
+        result = <li> do now : {tusk} time : {displayTime }</li>
+    }
+    return result
 }
